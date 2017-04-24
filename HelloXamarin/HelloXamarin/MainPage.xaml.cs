34b895cb729723a78fb243b172627aca6e0bca74
apspot <aradi.peter.spot@gmail.com>
2017-04-24 19:58:12 +0200

    Main menu amendments

--- HelloXamarin/HelloXamarin/MainPage.xaml.cs
+++ HelloXamarin/HelloXamarin/MainPage.xaml.cs
@@ -17,7 +17,7 @@ namespace HelloXamarin
                     new MainMenuItem("XAML essentials excercise (quotes)", () => { return new QuotesPage(); }),
                     new MainMenuItem("Stack layout excercise 1", () => { return new StackExcercise1Page(); }),
                     new MainMenuItem("Stack layout excercise 2", () => { return new StackExcercise2Page(); }),
-                    new MainMenuItem("Grid excercise 1", () => { return new GridExcercise1Page(); }),
+                    new MainMenuItem("Grid excercise 1 + style excercise", () => { return new GridExcercise1Page(); }),
                     new MainMenuItem("Grid excercise 2", () => { return new GridExcercise2Page(); }),
                     new MainMenuItem("Absolute layout excercise 1", () => { return new AbsoluteExcercise1Page(); }),
                     new MainMenuItem("Absolute layout excercise 2", () => { return new AbsoluteExcercise2Page(); }),
